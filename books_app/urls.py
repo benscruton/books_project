3250d7cf49342f21adbbb8cc6fcd21db9cc62cfe
Becca Theismann <becca@theismannconsulting.com>
2021-03-03 14:33:54 -0500

    initial commit

--- books_app/urls.py
+++ books_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("books/add", views.add_book, name="add_book"),
+    path("books/locate", views.locate_book, name="locate_book"),
+    path("books/create", views.create_book, name="create_book"),
+    path("books/<int:book_id>", views.book_detail, name="book_detai")
 ]
--- books_app/views.py
+++ books_app/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Website works!")
+    return render(request, "dashboard.html")
+
+def add_book(request):
+    return render(request, "add_book.html")
+
+def create_book(reaquest, book_id):
+    return redirect(f"books/{book_id}")
+
+def book_detail(request):
+    return render(request, "book_detail.html")
