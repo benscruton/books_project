8e15a2784bb2c81f757f99a9157e59cc61995051
benscruton <benjamin.scruton@gmail.com>
2021-06-15 17:20:57 -0500

    improvements to mobile menu

--- books_app/static/script.js
+++ books_app/static/script.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-});
+console.log(this.window);
+
+const toggleMobileMenu = () => {
+    let menu = document.getElementById("mobilemenu");
+    menu.classList = [
+        (menu.classList.contains("mobilemenuhidden") ? "mobilemenushown" : "mobilemenuhidden")
+    ];
+    menu.classList.add("hide-on-large-only");
+};
