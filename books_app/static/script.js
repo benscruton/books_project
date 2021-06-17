e7b815fb5f891f58b6fd8f56938abf20faffae01
benscruton <benjamin.scruton@gmail.com>
2021-06-17 09:39:26 -0500

    added collection styling to mobile menu

--- books_app/static/script.js
+++ books_app/static/script.js
@@ -6,4 +6,6 @@ const toggleMobileMenu = () => {
         (menu.classList.contains("mobilemenuhidden") ? "mobilemenushown" : "mobilemenuhidden")
     ];
     menu.classList.add("hide-on-large-only");
+    menu.classList.add("collection");
+    menu.classList.add("light-blue");
 };
